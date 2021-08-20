7d3436ee3bd63b6e8b6477133d05992e2ceb2cdc
Devon Wolfkiel <devon.wolfkiel@gmail.com>
2021-08-20 14:58:06 -0700

    Added dynamic request logic, started laying groudwork for accepting request body and token

--- src/hooks/useRequestForm.ts
+++ src/hooks/useRequestForm.ts
@@ -1,9 +1,11 @@
 import { FormEvent, ChangeEvent, useState } from 'react';
-import { getRequest } from '../services/fetch-utils';
+import { makeRequest } from '../services/fetch-utils';
 
 const useRequestForm = (method: string) => {
     const [results, setResults] = useState<unknown>(null);
     const [address, setAddress] = useState('');
+    const [body, setBody] = useState(null); 
+    const [token, setToken] = useState('');
 
     const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
         setAddress(e.target.value);
@@ -12,7 +14,7 @@ const useRequestForm = (method: string) => {
     const handleFormSubmit = async (e: FormEvent) => {
         e.preventDefault();
         console.log(method);
-        let response = await getRequest(address);
+        let response = await makeRequest(method, address, JSON.stringify(body), token);
         setResults(response || null);
         setAddress('');
     };
--- src/services/fetch-utils.ts
+++ src/services/fetch-utils.ts
@@ -1,9 +1,32 @@
-export const getRequest = async (address: string, token?: string): Promise<unknown[] | void> => {
+export const getRequest = async (address: string, token?: string): Promise<JSON | void> => {
     try {
         const response = await fetch(address);
         const json = await response.json();
-        if (Array.isArray(json)) return json;
-        else return [json]
+        return json;
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+// adapted from my own code here: https://github.com/devon-wolf/RESTlr/blob/main/src/services/api-utils.js
+export const makeRequest = async (method: string, address: string, body?: string, token = ''): Promise<JSON | void> => {
+    try {
+        let response;
+        if (method === 'GET') {
+            response = await fetch(address);
+        }
+        else {
+            response = await fetch(address, {
+                method,
+                body: body,
+                headers: { 
+                    'Content-Type': 'application/json',
+                    'Authorization': token
+                },
+            });
+        }
+        const json = await response.json();
+        return json;
     }
     catch (error) {
         console.log(error);
